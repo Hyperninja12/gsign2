2c39609fe24944a65d39dc4683e4f4ed76ebab18
Hyperninja12 <111427516+Hyperninja12@users.noreply.github.com>
2024-09-26 21:39:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+console.log('script.js is loaded');
+
 function onSignIn(googleUser) {
-    // Get the ID token from the Google User object
     const profile = googleUser.getBasicProfile();
     const id_token = googleUser.getAuthResponse().id_token;
 
@@ -7,11 +8,13 @@ function onSignIn(googleUser) {
     console.log('ID: ' + profile.getId());
     console.log('Full Name: ' + profile.getName());
     console.log('Email: ' + profile.getEmail());
-    console.log('ID Token: ' + id_token); // Log the ID token for verification
+    console.log('ID Token: ' + id_token);
 
+    // Check if the ID token is valid before redirecting
     if (id_token) {
         console.log('Redirecting...');
-        window.location.href = 'https://hyperninja12.github.io/Ash_Porfolio/portfolio.html'; 
+        // Use the page you want to redirect to after sign-in
+        window.location.href = 'https://hyperninja12.github.io/Ash_Porfolio/'; 
     } else {
         console.error('ID Token is missing. User may not be signed in properly.');
     }
